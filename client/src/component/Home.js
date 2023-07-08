4b652eb65876eaa12fb183645c1bb43cbf9a1031
OmarAmin10 <omar.seifallah@student.guc.edu.eg>
2023-07-08 22:19:13 +0300

    First Project

---
+++ client/src/component/Home.js
@@ -0,0 +1,34 @@
+import 'antd/dist/antd.css';
+import {useSelector,serSelector,useStore} from 'react-redux';
+import Header from "./Header";
+import SearchHome from "../component/SearchHome";
+import ImageCard from "./ImageCard";
+
+import CardForHome from './CardForHome';
+import FAQS from './FAQS';
+
+const Home =()=>{
+  
+  const styles = {
+    width:'100rem'
+};
+    return (
+      <div>
+      <div>
+        <Header/>
+        <div>
+        </div>
+        </div>
+        <div className="container-fluid h1 p-5 text-center">
+            <div className='col'>
+            </div>
+<SearchHome/>
+             </div>
+<div className='container'>
+  <CardForHome/>
+</div>
+
+             </div>
+    )
+};
+export default Home;
